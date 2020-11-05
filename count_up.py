176386a4a35f9c943503f37c55ca34437b0ebcee
Aida Tafrishi <atafr002@parrot.ib.hpcc.ucr.edu>
2020-11-05 10:39:44 -0800

    Second question edited

--- count_up.py
+++ count_up.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 
-# this is a python script template
-# this next line will download the file using curl
+
+from __future__ import division
 
 gff="Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.37.gff3.gz"
 fasta="Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz"
 
 import os,gzip,itertools,csv,re
 
-# this is code which will parse FASTA files
-# define what a header looks like in FASTA format
 def isheader(line):
     return line[0] == '>'
 
@@ -31,11 +29,34 @@ if not os.path.exists(gff):
 
 if not os.path.exists(fasta):
     os.system("curl -O ftp://ftp.ensemblgenomes.org/pub/bacteria/release-45/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/dna/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz")
-    
+
+ch_length = 0
+with gzip.open(fasta,"r") as f:
+    pairs = aspairs(f)
+    seqs  = dict(pairs)
+    for k,v in seqs.items():
+        ch_length += len(v)
+    print("Chromosome length is: %d " %(ch_length)) 
+
+
+gene_count = 0    
+sum_length = 0
+CDS_length = 0
+
 with gzip.open(gff,"rt") as fh:
-    # now add code to process this
     gff = csv.reader(fh,delimiter="\t")
     for row in gff:
         if row[0].startswith("#"):
             continue
-        print(row[3],row[6])
+        if row[2] == "gene":
+            gene_count = gene_count + 1
+            length = int(row[4]) - int(row[3])
+            sum_length = sum_length + length
+        if row[2] == "CDS":
+            length = int(row[4]) - int(row[3])
+            CDS_length = CDS_length + length
+    print("Total number of genes: %s " %(str(gene_count)))
+    print("Total number of nucleotides in the CDS: %s" %(str(sum_length)))
+
+
+print("The percentage of genome which is coding: %f" %((CDS_length/ch_length)*100))
